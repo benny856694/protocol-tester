ae8b7a496ff3a24a268a67c12e7b372523669406
shenbin <shenbinsc@qq.com>
2024-01-27 20:30:02 +0800

    checkin

--- frontend/src/pages/HttpServer.tsx
+++ frontend/src/pages/HttpServer.tsx
@@ -42,37 +42,44 @@ export default function HttpServer({ onNewRecord }: { onNewRecord?: (r: CaptureR
 
     return (
         <div className='border h-full flex flex-col rounded overflow-y-auto'>
-            <ScrollArea className=''>
-                {records.length > 0 && <Table className='relative'>
-                    {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-                    <TableHeader >
-                        <TableRow className='sticky top-0'>
-                            <TableHead className="">Time</TableHead>
-                            <TableHead>Name</TableHead>
-                            <TableHead>ID</TableHead>
-                            <TableHead>Sequence</TableHead>
-                            <TableHead className="">Device SN</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {records.map((r) => (
-                            <TableRow key={r.sequence_no + r.device_sn}>
-                                <TableCell className="font-medium">{r.cap_time}</TableCell>
-                                <TableCell>{r.match?.person_name}</TableCell>
-                                <TableCell>{r.match?.person_id}</TableCell>
-                                <TableCell>{r.sequence_no}</TableCell>
-                                <TableCell className="">{r.device_sn}</TableCell>
+            <div className='p-2 col-span-2'>
+                服务器监听端口8080
+            </div>
+            <div className='flex-1 overflow-y-auto flex flex-row'>
+                <ScrollArea className='self-start flex-[2_2_0%] h-full'>
+                    {records.length > 0 && <Table className='relative'>
+                        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+                        <TableHeader >
+                            <TableRow className='sticky top-0'>
+                                <TableHead className="">Time</TableHead>
+                                <TableHead>Name</TableHead>
+                                <TableHead>ID</TableHead>
+                                <TableHead>Sequence</TableHead>
+                                <TableHead className="">Device SN</TableHead>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                    {/* <TableFooter>
+                        </TableHeader>
+                        <TableBody>
+                            {records.map((r) => (
+                                <TableRow key={r.sequence_no + r.device_sn}>
+                                    <TableCell className="font-medium">{r.cap_time}</TableCell>
+                                    <TableCell>{r.match?.person_name}</TableCell>
+                                    <TableCell>{r.match?.person_id}</TableCell>
+                                    <TableCell>{r.sequence_no}</TableCell>
+                                    <TableCell className="">{r.device_sn}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        {/* <TableFooter>
                         <TableRow>
                             <TableCell colSpan={3}>Total</TableCell>
                             <TableCell className="text-right">{records.length}</TableCell>
                         </TableRow>
                     </TableFooter> */}
-                </Table>}
-            </ScrollArea>
+                    </Table>}
+                </ScrollArea>
+                <div className='flex-1 h-full border w-full'></div>
+            </div>
+
         </div>
     )
 }
