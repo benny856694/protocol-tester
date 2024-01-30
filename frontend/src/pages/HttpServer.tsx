86e89baa7db2db7a7ba677f386707edd03d4a64d
shenbin <shenbinsc@qq.com>
2024-01-30 11:11:55 +0800

    display images in table row

--- frontend/src/pages/HttpServer.tsx
+++ frontend/src/pages/HttpServer.tsx
@@ -17,11 +17,13 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { atom, useAtom } from 'jotai';
 import { JsonView } from 'react-json-view-lite';
 import JsonViewer from '@/components/json-viewer';
-import { normalizeImageData } from '@/lib/utils';
+import { cn, normalizeImageData } from '@/lib/utils';
 import { ReaderIcon } from '@radix-ui/react-icons';
 import { Toggle } from '@/components/ui/toggle';
 import { useTranslation } from 'react-i18next';
 
+const smallThumbnailSize = 32
+const bigThumbnailSize = 100
 
 const recordsAtomConfig = atom<CaptureRecord[]>([])
 const selRecordAtomConfig = atom<CaptureRecord | null>(null)
@@ -68,6 +70,7 @@ export default function HttpServer({ onNewRecord }: { onNewRecord?: (r: CaptureR
                         <TableHeader >
                             <TableRow className='sticky top-0'>
                                 <TableHead className="">Time</TableHead>
+                                <TableHead className="">Images</TableHead>
                                 <TableHead>Name</TableHead>
                                 <TableHead>ID</TableHead>
                                 <TableHead>Sequence</TableHead>
@@ -78,6 +81,10 @@ export default function HttpServer({ onNewRecord }: { onNewRecord?: (r: CaptureR
                             {records.map((r) => (
                                 <TableRow onClick={() => setSelRecord(r)} key={r.sequence_no + r.device_sn}>
                                     <TableCell className="font-medium">{r.cap_time}</TableCell>
+                                    <TableCell className={cn('w-full flex flex-row  gap-4')}>
+                                        {r.closeup_pic?.data && <img className='rounded' src={normalizeImageData(r.closeup_pic?.data)} alt="closeup" width={smallThumbnailSize} height={smallThumbnailSize} />}
+                                        {r.match?.image && <img className='rounded' src={normalizeImageData(r.match?.image)} alt="template" width={smallThumbnailSize} height={smallThumbnailSize} />}
+                                    </TableCell>
                                     <TableCell>{r.match?.person_name}</TableCell>
                                     <TableCell>{r.match?.person_id}</TableCell>
                                     <TableCell>{r.sequence_no}</TableCell>
@@ -97,9 +104,9 @@ export default function HttpServer({ onNewRecord }: { onNewRecord?: (r: CaptureR
                 {
                     detailsVisible && (
                         <div className='flex-1 flex flex-col overflow-auto rounded border p-2'>
-                            <div className='max-h-[64px] w-full flex flex-row justify-center gap-4'>
-                                {selRecord?.closeup_pic?.data && <img src={normalizeImageData(selRecord?.closeup_pic?.data)} alt="closeup" width={64} height={64} />}
-                                {selRecord?.match?.image && <img src={normalizeImageData(selRecord?.match?.image)} alt="template" width={64} height={64} />}
+                            <div className={cn(' w-full flex flex-row justify-center gap-4')}>
+                                {selRecord?.closeup_pic?.data && <img className='rounded' src={normalizeImageData(selRecord?.closeup_pic?.data)} alt="closeup" width={bigThumbnailSize} height={bigThumbnailSize} />}
+                                {selRecord?.match?.image && <img className='rounded' src={normalizeImageData(selRecord?.match?.image)} alt="template" width={bigThumbnailSize} height={bigThumbnailSize} />}
                             </div>
                             {!selRecord && <p className='flex justify-center items-center h-full text-center'>{t('clickOneRowToViewDetail')}</p>}
                             {selRecord && <ScrollArea>
